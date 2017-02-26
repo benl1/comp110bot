8c140a665201f28c682d9d535fcbbaa2a0e30e0d
benl1 <benjaminL12345@gmail.com>
2017-02-25 23:16:46 -0500

    stuff

--- app.js
+++ app.js
@@ -12,19 +12,17 @@ app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
  
 app.listen(port, function () {
   console.log('Listening on port ' + port);
-});
+}
 
-
-app.post('hello', function(req, res, next){
-	var userName = req.body.user_name;
-	var botPayload = {
-		text: 'hello' + userName + ', welcome my friend)'
-	};
-	
-	if (userName !== 'slackbot'){
-		return res.status(200).json(botPayload)
-		
-	} else{
-		return res.status(200).end();
-	}
+app.post('/hello', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName + ', welcome to Devdactic Slack channel! I\'ll be your guide.'
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
 });
